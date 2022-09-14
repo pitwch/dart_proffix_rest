f87be036bbdf36112ec684e382332a1c86fdecba
sp <s.pedrett@pitw.ch>
2022-09-14 18:20:30 +0200

    Fix: Change Test API

--- test/dart_proffix_rest_test.dart
+++ test/dart_proffix_rest_test.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import "package:crypto/crypto.dart";
 import 'package:dart_proffix_rest/dart_proffix_rest.dart';
 import 'package:test/test.dart';
+import 'dart:io' show Platform;
 
 // Generating code coverage:
 // 1. `dart pub global activate coverage`
@@ -13,12 +14,16 @@ import 'package:test/test.dart';
 
 void main() {
   test('Check Login', () async {
-    var bytesToHash = utf8.encode('gast123');
+    Map<String, String> envVars = Platform.environment;
+
+    var bytesToHash = utf8.encode(
+      envVars['PX_PASS'].toString(),
+    );
     var sha256Digest = sha256.convert(bytesToHash);
     var tempClient = ProffixClient(
         database: 'DEMODB',
-        restURL: 'https://remote.proffix.net:11011',
-        username: 'Gast',
+        restURL: envVars['PX_URL'].toString(),
+        username: envVars['PX_USER'].toString(),
         password: sha256Digest.toString());
 
     var request =
